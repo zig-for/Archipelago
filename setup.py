8c0624329d211fff840d788fdd592b00b8edd684
Fabian Dill <fabian.dill@web.de>
2020-03-03 14:02:12 +0100

    add new resources folder to compile output because there's already a data folder and you can always use more of those

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ def installfile(path):
         print('Warning,', path, 'not found')
 
 
-extra_data = ["LICENSE", "data", "EnemizerCLI", "host.yaml", "QUsb2Snes", "meta.yaml"]
+extra_data = ["LICENSE", "data", "EnemizerCLI", "host.yaml", "QUsb2Snes", "meta.yaml", "resources"]
 
 for data in extra_data:
     installfile(Path(data))
