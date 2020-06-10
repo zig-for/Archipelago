51d35f8d08466189b671293f41a0949e3c6aac33
Fabian Dill <fabian.dill@web.de>
2020-06-10 06:17:10 +0200

    remove pinball key if keyshuffle

--- Dungeons.py
+++ Dungeons.py
@@ -42,7 +42,7 @@ def fill_dungeons(world):
     all_state_base = world.get_all_state()
 
     for player in range(1, world.players + 1):
-        if not world.retro[player] and world.logic == "noglitches":
+        if not world.retro[player] and world.logic == "noglitches" and not world.keyshuffle[player]:
             pinball_room = world.get_location('Skull Woods - Pinball Room', player)
             world.push_item(pinball_room, ItemFactory('Small Key (Skull Woods)', player), False)
             pinball_room.event = True
@@ -118,15 +118,14 @@ def get_dungeon_item_pool(world):
 def fill_dungeons_restrictive(world, shuffled_locations):
     all_state_base = world.get_all_state()
 
-    # for player in range(1, world.players + 1):
-    #     pinball_room = world.get_location('Skull Woods - Pinball Room', player)
-    #     if world.retro[player]:
-    #         world.push_item(pinball_room, ItemFactory('Small Key (Universal)', player), False)
-    #     else:
-    #         world.push_item(pinball_room, ItemFactory('Small Key (Skull Woods)', player), False)
-    #     pinball_room.event = True
-    #     pinball_room.locked = True
-    #     shuffled_locations.remove(pinball_room)
+    for player in range(1, world.players + 1):
+        if not world.retro[player] and world.logic == "noglitches" and not world.keyshuffle[player]\
+                and world.doorShuffle[player] == "vanilla":
+            pinball_room = world.get_location('Skull Woods - Pinball Room', player)
+            world.push_item(pinball_room, ItemFactory('Small Key (Skull Woods)', player), False)
+            pinball_room.event = True
+            pinball_room.locked = True
+            shuffled_locations.remove(pinball_room)
 
     # with shuffled dungeon items they are distributed as part of the normal item pool
     for item in world.get_items():
