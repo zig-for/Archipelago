dowlle/ctr-archipelago-apworld

Adjust output to "BM" (Berserker's Multiworld) to differentiate spoiler logs, roms and server data at a glance Rename _multidata to .multidata and embed .multidata as MultiServer-openable file into windows registry

Main.py
@@ -152,7 +152,7 @@ def main(args, seed=None):
 
     logger.info('Patching ROM.')
 
-    outfilebase = 'ER_%s' % (args.outputname if args.outputname else world.seed)
+    outfilebase = 'BM_%s' % (args.outputname if args.outputname else world.seed)
 
     rom_names = []
     jsonout = {}
@@ -269,7 +269,7 @@ def main(args, seed=None):
         if args.jsonout:
             jsonout["multidata"] = list(multidata)
         else:
-            with open(output_path('%s_multidata' % outfilebase), 'wb') as f:
+            with open(output_path('%s.multidata' % outfilebase), 'wb') as f:
                 f.write(multidata)
 
     if not args.skip_playthrough:

MultiMystery.py
@@ -118,8 +118,8 @@ if __name__ == "__main__":
                 seedname = segment
                 break
 
-        multidataname = f"ER_{seedname}_multidata"
-        spoilername = f"ER_{seedname}_Spoiler.txt"
+        multidataname = f"BM_{seedname}.multidata"
+        spoilername = f"BM_{seedname}_Spoiler.txt"
         romfilename = ""
 
         if player_name:

zig-for/Archipelago

Main.py
@@ -194,7 +194,7 @@ def main(args, seed=None, fish=None):
     if not world.can_beat_game():
         raise RuntimeError(world.fish.translate("cli","cli","cannot.beat.game"))
 
-    outfilebase = 'DR_%s' % (args.outputname if args.outputname else world.seed)
+    outfilebase = 'BMD_%s' % (args.outputname if args.outputname else world.seed)
 
     rom_names = []
     jsonout = {}
@@ -346,7 +346,7 @@ def main(args, seed=None, fish=None):
         if args.jsonout:
             jsonout["multidata"] = list(multidata)
         else:
-            with open(output_path('%s_multidata' % outfilebase), 'wb') as f:
+            with open(output_path('%s.multidata' % outfilebase), 'wb') as f:
                 f.write(multidata)
 
     if not args.skip_playthrough:

MultiMystery.py
@@ -300,8 +300,8 @@ if __name__ == "__main__":
 
         print()
         print(f"Took {time.perf_counter()-start:.3f} seconds to generate rom(s).")
-        multidataname = f"DR_{seedname}_multidata"
-        spoilername = f"DR_{seedname}_Spoiler.txt"
+        multidataname = f"BMD_{seedname}.multidata"
+        spoilername = f"BMD_{seedname}_Spoiler.txt"
         romfilename = ""
 
         if player_name: