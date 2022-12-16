502d9a8edc7b60de0dfe20e3511df7c20bcbd615
Kyle Franz <san.francisco.engineer@gmail.com>
2022-12-15 21:54:05 -0800

    wip

--- worlds/la/Locations.py
+++ worlds/la/Locations.py
@@ -1,1 +1,17 @@
-import foobar
+from __future__ import absolute_import
+
+import sys
+from os.path import dirname
+sys.path.append(dirname(__file__) + "/LADXR")
+
+from LADXR.logic.overworld import World as LADXRWorld
+from LADXR.settings import Settings as LADXRSettings
+from LADXR.worldSetup import WorldSetup as LADXRWorldSetup
+from LADXR.logic.requirements import RequirementsSettings
+
+def generate_reference_locations():
+    options =  LADXRSettings()
+    world_setup = LADXRWorldSetup()
+    r = RequirementsSettings()
+    world = LADXRWorld(options = options, world_setup = world_setup, r = r)
+    
--- worlds/la/__init__.py
+++ worlds/la/__init__.py
@@ -1,11 +1,51 @@
-from ..AutoWorld import World
-from Common import *
-# Link's Awakening port by zig
-# Original Link's Awakening DX Rando by daid and otherst
-# https://github.com/daid/LADXR
+# world/mygame/__init__.py
+
+from .Options import links_awakening_options  # the options we defined earlier
+from .Items import LinksAwakeningItem, item_list  # data used below to add items to the World
+from .Locations import generate_reference_locations
+from worlds.AutoWorld import World
+from BaseClasses import Region, Location, Entrance, Item, RegionType, ItemClassification
+from Utils import get_options, output_path
+from .Common import *
+
+class LinksAwakeningLocation(Location):  # or from Locations import MyGameLocation
+    game = LINKS_AWAKENING  # name of the game/world this location is in
+
+BASE_ID = 10000000
 
 class LinksAwakeningWorld(World):
-    game: str = LINKS_AWAKENING # TODO: how is this used? Is conciseness valued?
-    topology_present = True
+    """Insert description of the world/game here."""
+    game: str = LINKS_AWAKENING # name of the game/world
+    option_definitions = links_awakening_options  # options the player can set
+    topology_present: bool = True  # show path to required location checks in spoiler
 
+    # data_version is used to signal that items, locations or their names
+    # changed. Set this to 0 during development so other games' clients do not
+    # cache any texts, then increase by 1 for each release that makes changes.
+    data_version = 0
 
+    # ID of first item and location, could be hard-coded but code may be easier
+    # to read with this as a propery.
+    base_id = BASE_ID
+    # Instead of dynamic numbering, IDs could be part of data.
+
+    # The following two dicts are required for the generation to know which
+    # items exist. They could be generated from json or something else. They can
+    # include events, but don't have to since events will be placed manually.
+    item_name_to_id = {
+        item.item_name : BASE_ID + item.item_id for item in item_list
+    }#{name: id for
+                      # id, name in enumerate(links_awakening_items, base_id)}
+
+    reference_locations = generate_reference_locations()
+    location_name_to_id = {}
+    #location_name_to_id = {name: id for
+    #                       id, name in enumerate(mygame_locations, base_id)}
+
+    # Items can be grouped using their names to allow easy checking if any item
+    # from that group has been collected. Group names can also be used for !hint
+    #item_name_groups = {
+    #    "weapons": {"sword", "lance"}
+    #}
+
+    
